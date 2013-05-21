85f441ab5f11160ec2bd2fb92cc42210624480cb
pwoods <pwoods@usacl-pwoods.(none)>
2013-05-21 17:29:51 -0400

    functional non JS app.  Mostly functional JS app

--- app.py
+++ app.py
@@ -1,22 +1,95 @@
 import os
-from bottle import Bottle, run, template, static_file
+import shelve
+import itertools
+from time import sleep
+from bottle import Bottle, run, template, static_file, request
+from bottle import redirect
 
 
 app_path = os.path.dirname(os.path.realpath(__file__))
+shelf_file = os.path.join(app_path, 'shelf')
+static_root = os.path.join(app_path, 'static')
 
 app = Bottle()
 
 @app.get('/')
 def index():
-    return template('index.html')
+    db = shelve.open(shelf_file)
+    entries = dict(db)
+    entry_list = EntryList(entries)
+    db.close()
+    success = u'Congratulations! You have things to do.'
+    return template('index.html', entries=entry_list, success=success, has_entries=True)
 
 @app.post('/')
-def submit_entry():
-    return 'A Post'
+def add_entry():
+    entry = request.forms.entry
+    if not entry:
+        error = u'Blank entries are not supported'
+        return template('index.html', error=error)
+    db = shelve.open(shelf_file)
+    new_entry = Entry(entry)
+    db[id] = new_entry
+    db.close()
+    sleep(5)
+    redirect('/')
+
+@app.post('/complete')
+def complete():
+    task_ids = request.forms.getall('task')
+    db = shelve.open(shelf_file)
+    for id in task_ids:
+        tsk = db[id]
+        tsk.complete = True
+        db[id] = tsk
+    db.close()
+    sleep(5)
+    redirect('/')
 
 @app.route('/static/<filename:path>')
 def send_static(filename):
-    static_root = os.path.join(app_path, 'static')
     return static_file(filename, root=static_root)
+
+class EntryList(object):
+    def __init__(self, entries):
+        self.entries = entries
+        self.groups = {}
+        grouped = itertools.groupby(entries.values(), lambda x: x.status)
+        for k, g in grouped:
+            self.groups[k] = list(g)
+
+    @property
+    def pending(self):
+        return self.groups.get('Pending', [])
+
+    @property
+    def complete(self):
+        return self.groups.get('Complete', [])
+
+    def __len__(self):
+        return len(self.entries)
+
+class Entry(object):
+    complete = False
+    message = u''
+    complete = False
+
+    @property
+    def status(self):
+        if self.complete:
+            return u'Complete'
+        else:
+            return u'Pending'
+
+    def __init__(self, text, id):
+        self.message = text
+    
+    @property
+    def id(self):
+        return "-".join(self.message.split())
+        
+    def __unicode__(self):
+        return self.message
+
 
 run(app, host='localhost', port=8080, debug=True, reloader=True)
--- static/js/todo.js
+++ static/js/todo.js
@@ -1,66 +1,17 @@
-var Todo = window.Todo || {};
 
 
+;(function (window) {
 
-Todo.List = function (addForm, itemsForm, bindListeners) {
-	this.addForm = document.getElementById(addForm);
-	this.itemsForm = document.getElementById(itemsForm);
-	if (bindListeners) {
-		this.bindListeners();
-	}
-	console.log(addForm);
-};
+window.Todo = window.Todo || {};
 
-Todo.List.prototype.bindListeners = function () {
-	this.addForm.addEventListener('submit', this.bindAsListener(this.addItem, this), false);
-};
-
-Todo.List.prototype.addItem = function (evt) {
-	evt.preventDefault();
-	var entry = document.getElementById('entry');
-	if (entry && entry.value) {
-		var item = this.buildItem(entry.value);
-	}
-	this.itemsForm.appendChild(item);
-	item.addEventListener('click', this.bindAsListener(this.complete, this), false);
-};
-
-Todo.List.prototype.buildItem = function (entry) {
-	var container = this.el('div');
-	var label = this.el('label', {
-		'class': 'checkbox'
-	});
-	var input = this.el('input', {
-		'type':'checkbox'
-	});
-	var entryNode = document.createTextNode(entry);
-	label.appendChild(input);
-	label.appendChild(entryNode);
-	container.appendChild(label);
-	return container;
-	
-};
-
-Todo.List.prototype.el = function (name, attrs) {
-	var el = document.createElement(name);
-	for (var att in attrs) {
-		el.setAttribute(att, attrs[att]);
-	}
-	return el;
-};
-
-Todo.List.prototype.has = function (obj, prop) {
+var has = function (obj, prop) {
 	return Object.prototype.hasOwnProperty.call(obj, prop);
 };
 
-Todo.List.prototype.bindAsListener = function (fn, scope) {
+var bindAsListener = function (fn, scope) {
 	return function () {
 		return fn.apply(scope, arguments);
 	};
-};
-
-Todo.List.prototype.complete = function (evt) {
-	console.log(evt.target.matches('label'));
 };
 
 var findPrefixed = function (fn) {
@@ -72,6 +23,124 @@ var findPrefixed = function (fn) {
 			return el[prefixed];
 		}
 	}
-}
+	return null;
+};
+var matchFn = findPrefixed('MatchesSelector');
+var matches = function(element, selector) {
+	return matchFn.call(element, selector);
+};
+
+var el = function (name, attrs) {
+	var el = document.createElement(name);
+	for (var att in attrs) {
+		el.setAttribute(att, attrs[att]);
+	}
+	return el;
+};
+
+var txt = function(text) {
+	return document.createTextNode(text);
+};
+
+window.ajax = function (options) {
+	var request = function () {
+		if (window.XMLHttpRequest) { // Mozilla, Safari, ...
+			return new XMLHttpRequest();
+		} else if (window.ActiveXObject) { // IE 8 and older
+			return new ActiveXObject("Microsoft.XMLHTTP");
+		}
+		// I give up
+		return false;
+	};
+	
+	var httpRequest = request();
+	var process = function (process) {
+		var httpRequest = process.currentTarget;
+		if (httpRequest.readyState === 4) {
+			if (httpRequest.status === 200) {
+				console.log(httpRequest.responseText);
+			} else {
+				alert('There was a problem with the request.');
+			}
+		}
+	};
+
+	httpRequest.onreadystatechange = process;
+    httpRequest.open('GET', '/');
+    httpRequest.send();
+};
+
+var List = function (addForm, itemsForm, bindListeners) {
+	this.addForm = document.getElementById(addForm);
+	this.itemsForm = document.getElementById(itemsForm);
+	if (bindListeners) {
+		this.bindListeners();
+	}
+};
+
+List.prototype.bindListeners = function () {
+	this.addForm.addEventListener('submit', bindAsListener(this.addItem, this), false);
+	var inputs = document.getElementsByTagName('input');
+	console.log(inputs.length);
+	for (var i = 0; i < inputs.length; i++) {
+		var input = inputs[i];
+		if (input.type == 'checkbox' && !input.checked) {
+			input.addEventListener('click', bindAsListener(this.complete, this), false);
+		}
+	}
+};
+
+List.prototype.addItem = function (evt) {
+	evt.preventDefault();
+	var entry = document.getElementById('entry');
+	if (entry && entry.value) {
+		var item = this.buildItem(entry.value);
+	}
+	this.itemsForm.appendChild(item);
+	item.addEventListener('click', bindAsListener(this.complete, this), false);
+};
+
+List.prototype.buildItem = function (entry) {
+	var container = el('div');
+	var label = el('label', {
+		'class': 'checkbox'
+	});
+	var input = el('input', {
+		'type':'checkbox'
+	});
+	var entryNode = txt(entry);
+	label.appendChild(input);
+	label.appendChild(entryNode);
+	container.appendChild(label);
+	return container;
+	
+};
+
+List.prototype.complete = function (evt) {
+	var input = evt.target;
+	var label = input.parentNode;
+	var new_label = label;
+	var text = new_label.textContent;
+	var strike = el('strike');
+	strike.appendChild(txt(text));
+	new_label.removeChild(label.lastChild);
+	new_label.appendChild(strike);
+	var complete = document.getElementById('entries-complete');
+	complete.appendChild(new_label);
+	//label.parentNode.removeChild(label);
+	var pending = document.getElementById('entries-pending');
+	console.log(pending.children.length);
+	if (pending.children.length < 2) {
+		pending.style.display = 'none';
+	}
+	
+};
+
+
+Todo.List = List;
+
+})(window);
+
 
 new Todo.List('add-form', 'items-form', true);
+document.body.setAttribute('class', 'js');
