7cae906b5143f57d332a3bb0a4abcbba5e7e4808
hakjoon <public@hakjoon.com>
2013-05-21 22:41:18 -0400

    all working

--- app.py
+++ app.py
@@ -16,7 +16,9 @@ app = Bottle()
 def index():
     db = shelve.open(shelf_file)
     entries = dict(db)
+    print entries
     entry_list = EntryList(entries)
+    print entry_list.complete
     db.close()
     success = u'Congratulations! You have things to do.'
     return template('index.html', entries=entry_list, success=success, has_entries=True)
@@ -28,11 +30,15 @@ def add_entry():
         error = u'Blank entries are not supported'
         return template('index.html', error=error)
     db = shelve.open(shelf_file)
-    new_entry = Entry(entry)
-    db[id] = new_entry
+    id = str(len(db))
+    new_entry = Entry(entry, id)
+    db[new_entry.id] = new_entry
     db.close()
-    sleep(5)
-    redirect('/')
+    #sleep(5)
+    if request.query.ajax:
+        return new_entry.id
+    else:
+        redirect('/')
 
 @app.post('/complete')
 def complete():
@@ -43,7 +49,7 @@ def complete():
         tsk.complete = True
         db[id] = tsk
     db.close()
-    sleep(5)
+    #sleep(5)
     redirect('/')
 
 @app.route('/static/<filename:path>')
@@ -53,18 +59,24 @@ def send_static(filename):
 class EntryList(object):
     def __init__(self, entries):
         self.entries = entries
-        self.groups = {}
-        grouped = itertools.groupby(entries.values(), lambda x: x.status)
-        for k, g in grouped:
-            self.groups[k] = list(g)
+        self.groups = {
+            'pending':[v for v in entries.values() if not v.complete],
+            'complete':[v for v in entries.values() if v.complete]
+        }
 
     @property
     def pending(self):
-        return self.groups.get('Pending', [])
+        lst = self.groups.get('pending', [])
+        lst = sorted(lst, key=lambda x: x.id)
+        lst.reverse()
+        return lst
 
     @property
     def complete(self):
-        return self.groups.get('Complete', [])
+        lst = self.groups.get('complete', [])
+        lst = sorted(lst, key=lambda x: x.id)
+        lst.reverse()
+        return lst
 
     def __len__(self):
         return len(self.entries)
@@ -73,22 +85,20 @@ class Entry(object):
     complete = False
     message = u''
     complete = False
-
-    @property
-    def status(self):
-        if self.complete:
-            return u'Complete'
-        else:
-            return u'Pending'
+    id = None
 
     def __init__(self, text, id):
         self.message = text
-    
+        self.id = id
+
     @property
-    def id(self):
+    def slug(self):
         return "-".join(self.message.split())
-        
+
     def __unicode__(self):
+        return self.message
+
+    def __repr__(self):
         return self.message
 
 
--- static/js/todo.js
+++ static/js/todo.js
@@ -1,8 +1,11 @@
 
 
-;(function (window) {
+(function (global, d) {
 
-window.Todo = window.Todo || {};
+var $ = function (k) { return d.getElementById.call(d, k); };
+var $A = function (arrayLike) {
+	return Array.prototype.slice.call(arrayLike, 0);
+};
 
 var has = function (obj, prop) {
 	return Object.prototype.hasOwnProperty.call(obj, prop);
@@ -14,39 +17,82 @@ var bindAsListener = function (fn, scope) {
 	};
 };
 
-var findPrefixed = function (fn) {
-	var vendorPrefixes = ['webkit', 'moz', 'ms', 'o'];
-	var el = document.createElement('div');
-	for (var i=0; i< vendorPrefixes.length; i++) {
-		var prefixed = vendorPrefixes[i] + fn;
-		if (el[prefixed]) {
-			return el[prefixed];
-		}
-	}
-	return null;
-};
-var matchFn = findPrefixed('MatchesSelector');
-var matches = function(element, selector) {
-	return matchFn.call(element, selector);
-};
-
 var el = function (name, attrs) {
-	var el = document.createElement(name);
+	var el = d.createElement(name);
 	for (var att in attrs) {
 		el.setAttribute(att, attrs[att]);
 	}
 	return el;
 };
 
-var txt = function(text) {
-	return document.createTextNode(text);
+var findPrefixed = function (fn) {
+	var vendorPrefixes = ['webkit', 'moz', 'ms', 'o'];
+	var e = el('div');
+	for (var i=0; i< vendorPrefixes.length; i++) {
+		var prefixed = vendorPrefixes[i] + fn;
+		if (e[prefixed]) {
+			return e[prefixed];
+		}
+	}
+	return null;
 };
 
-window.ajax = function (options) {
+var matchFn = findPrefixed('MatchesSelector');
+var matches = function(element, selector) {
+	return matchFn.call(element, selector);
+};
+
+var txt = function(text) {
+	return d.createTextNode(text);
+};
+
+var addClass = function (element, theClass) {
+	var classes = element.getAttribute('class').split(" ");
+	classes.concat(theClass);
+	element.setAttribute('class', classes.join(" "));
+};
+
+var removeClass = function (element, theClass) {
+	var classes = element.getAttribute('class').split(" ");
+	var index = classes.indexOf(theClass);
+	if (index != -1) {
+		classes.splice(index, 1);
+	}
+	element.setAttribute('class', classes.join(" "));
+};
+
+var extend = function(defaults, overrides) {
+	if (!overrides) {
+		return defaults;
+	}
+	for (var property in overrides) {
+		defaults[property] = overrides[property];
+	}
+	return defaults;
+};
+
+var cloneObject = function (object) {
+	return extend({}, object);
+};
+
+var inString = function (str, match) {
+	var includes = str.indexOf(match);
+	return includes != -1;
+};
+
+var ajax = function (url, kwargs) {
+	var defaults = {
+		method: 'get',
+		success: function () {},
+		error: function () {}
+	};
+	options = extend(defaults, kwargs);
+	var method = options.method.toLowerCase();
+	var params = options.params;
 	var request = function () {
-		if (window.XMLHttpRequest) { // Mozilla, Safari, ...
+		if (global.XMLHttpRequest) { // Mozilla, Safari, ...
 			return new XMLHttpRequest();
-		} else if (window.ActiveXObject) { // IE 8 and older
+		} else if (global.ActiveXObject) { // IE 8 and older
 			return new ActiveXObject("Microsoft.XMLHTTP");
 		}
 		// I give up
@@ -54,50 +100,64 @@ window.ajax = function (options) {
 	};
 	
 	var httpRequest = request();
+	if (params && method === 'get') {
+		url += (inString(url, '?') ? '&' : '?') + params;
+	}
+	var body = (method === 'post') ? params : null;
+
 	var process = function (process) {
-		var httpRequest = process.currentTarget;
 		if (httpRequest.readyState === 4) {
 			if (httpRequest.status === 200) {
-				console.log(httpRequest.responseText);
+				options.success(httpRequest);
 			} else {
-				alert('There was a problem with the request.');
+				options.error(httpRequest);
 			}
+			httpRequest.onreadystatechange = function () {};
 		}
 	};
-
+	
 	httpRequest.onreadystatechange = process;
-    httpRequest.open('GET', '/');
-    httpRequest.send();
+    httpRequest.open(options.method.toUpperCase(), url);
+    httpRequest.send(body);
 };
+
+
+// -- TODO List 
+var todo = global.Todo = global.Todo || {};
 
 var List = function (addForm, itemsForm, bindListeners) {
 	this.addForm = document.getElementById(addForm);
 	this.itemsForm = document.getElementById(itemsForm);
+	this.itemsHolder = document.getElementById('entry-well');
+	this.emptyMessage = document.getElementById('empty-message');
 	if (bindListeners) {
 		this.bindListeners();
+		document.body.setAttribute('class', 'js');
 	}
+	
 };
 
 List.prototype.bindListeners = function () {
 	this.addForm.addEventListener('submit', bindAsListener(this.addItem, this), false);
 	var inputs = document.getElementsByTagName('input');
-	console.log(inputs.length);
-	for (var i = 0; i < inputs.length; i++) {
-		var input = inputs[i];
+	$A(inputs).forEach(function (input, i, arr) {
 		if (input.type == 'checkbox' && !input.checked) {
 			input.addEventListener('click', bindAsListener(this.complete, this), false);
 		}
-	}
+	}, this);
 };
 
 List.prototype.addItem = function (evt) {
 	evt.preventDefault();
+	var form = evt.target
 	var entry = document.getElementById('entry');
 	if (entry && entry.value) {
 		var item = this.buildItem(entry.value);
 	}
-	this.itemsForm.appendChild(item);
+	var fieldset = this.itemsForm.children[0];
+	fieldset.insertBefore(item, fieldset.firstChild);
 	item.addEventListener('click', bindAsListener(this.complete, this), false);
+	form.reset();
 };
 
 List.prototype.buildItem = function (entry) {
@@ -112,6 +172,13 @@ List.prototype.buildItem = function (entry) {
 	label.appendChild(input);
 	label.appendChild(entryNode);
 	container.appendChild(label);
+	ajax('/?ajax=True', {
+		params:'entry=' + entry,
+		method: 'post',
+		success: function (response) {
+			input.setAttribute('value', response.responseText);
+		}
+	});
 	return container;
 	
 };
@@ -125,22 +192,18 @@ List.prototype.complete = function (evt) {
 	strike.appendChild(txt(text));
 	new_label.removeChild(label.lastChild);
 	new_label.appendChild(strike);
-	var complete = document.getElementById('entries-complete');
-	complete.appendChild(new_label);
-	//label.parentNode.removeChild(label);
-	var pending = document.getElementById('entries-pending');
-	console.log(pending.children.length);
-	if (pending.children.length < 2) {
-		pending.style.display = 'none';
-	}
-	
+	new_label.parentNode.parentNode.appendChild(new_label);
+	ajax('/complete', {
+		params: input.name + '=' + input.value,
+		method:'post'
+	});
 };
 
 
-Todo.List = List;
+todo.List = List;
 
-})(window);
+})(window, document);
 
 
 new Todo.List('add-form', 'items-form', true);
-document.body.setAttribute('class', 'js');
+
